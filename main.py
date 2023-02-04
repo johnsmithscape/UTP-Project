29f30532932b6ac31bf959676fbc3f911d361f39
Ivan Kuznetsov <108028396+Vankinok@users.noreply.github.com>
2023-02-04 12:50:18 +0300

    v0.2
    NEW FEATURE:
    1. config.py (you can use your config)
    2. config build (this program can build config itself)
    
    Deleted all items except config.py and config builder

--- main.py
+++ main.py
@@ -1,181 +1,202 @@
 import os
-
+from config import *
 print('Добро пожаловать в UTP (Useful Tools Pack - Пак полезных инструментов)\n\n#      #  ########  ######\n#      #     ##     #     #\n#      #     ##     ######\n#      #     ##     #\n ######   ########  #\n\n(4pda.to, Vankinok, https://4pda.to/forum/index.php?showuser=9457591)')
-tool = int(input('Список доступных инструментов\n1. Прошивка рекавери\n2. Прошивка firmware\n3. Зашив прошивки(для recovery)\n4. Магиск\nЧтобы продолжить - напишите цифру от 1 до 4 => '))
+tool = int(input('1. config builder\n2. config.py\nЧтобы продолжить - напишите цифру 1 или 2 => '))
 if tool == 1:
-    
-    print('Вы выбрали - "Прошивка рекавери"')
-    print('Подключите ваше устройство в режиме fastboot')
-    
-    twrp = int(input('Список доступных кастомных рекавери\n1. twrp skkk a11\n2. twrp skkk a12\n3. twrp skkk a13\n4. orange fox recovery stable\n5. orange fox recovery beta\n6. свое рекавери (c именем recovery.img) => '))
-   
-    if twrp == 1:
 
-        print('Загрузка twrp skkk a11')
-        os.system("aria2c https://deac-fra.dl.sourceforge.net/project/recovery-for-xiaomi-devices/alioth/twrp-3.6.0_11-v3.1_A11-alioth-skkk.img")
-        os.system("fastboot boot twrp-3.6.0_11-v3.1_A11-alioth-skkk.img")
-        print('Чтобы установить тврп на постоянку:\n1. Переходил раздел advanced (допольнительные настойки)\n2. Жмем на flash current twrp (прошить текущий тврп)')
-        input('Готово (нажмите enter для выхода)')
-        exit(0)
-    
-    elif twrp == 2:
+    cfg_work = int(input("Какой инструмент вам нужен?\n1. прошивка recovery\n2. прошивка firmware\n3. зашив прошивки (recovery)\n4. magisk => "))
+    cfg_name = "config.txt"
+    if cfg_work == 1:
+        cfg_tool = ('work_type = "recovery"')
+        os.system("echo '" + cfg_tool + "' > $(pwd)/" + cfg_name)
+        cfg_rec = int(input("Какой тип рекавери вам нужен? (1. twrp, 2. ofox) => "))
+        
+        if cfg_rec == 1:
+            
+            twrp_type = ('recovery_type = "twrp"')
+            os.system("echo '" + twrp_type + "' >> $(pwd)/" + cfg_name)
+            cfg_rec_path = input("Укажите полный путь до образа рекавери (Например /home/tux/recovery.img) : ")
+            twrp_path = ('recovery_path = "' + cfg_rec_path + '"')
+            os.system("echo '" + twrp_path + "' >> $(pwd)/" + cfg_name)
+            cfg_rec_guide = input("Хотите ли вы, чтобы программа рассказала вам о том, как установить рекавери на постоянку (true/false) => ")
+            guide = ('show_guide = "' + cfg_rec_guide + '"')
+            os.system("echo '" + guide + "' >> $(pwd)/" + cfg_name)
+            os.system("mv " + cfg_name + " config.py")
+            input("\nГотово (нажмите enter для выхода)")
+            exit(0)
+        
+        elif cfg_rec == 2:
+            
+            ofox_type = ('recovery_type = "ofox"')
+            os.system("echo '" + ofox_type + "' >> $(pwd)/" + cfg_name)
+            cfg_rec_path = input("Укажите полный путь до установочного zip архива ofox (Например /home/tux/ofox.zip) : ")
+            ofox_path = ('recovery_path = "' + cfg_rec_path + '"')
+            os.system("echo '" + ofox_path + "' >> $(pwd)/" + cfg_name)
+            cfg_rec_unpack = input("Укажите каталог, который будет создан для распаковки архива ofox (Например /home/tux/utp/ofox) : ")
+            ofox_unpack = ('unpackfolder = "' + cfg_rec_unpack + '"')
+            os.system("echo '" + ofox_unpack + "' >> $(pwd)/" + cfg_name)
+            cfg_rec_guide = input("Хотите ли вы, чтобы программа рассказала вам о том, как установить рекавери на постоянку (true/false) => ")
+            guide = ('show_guide = "' + cfg_rec_guide + '"')
+            os.system("echo '" + guide + "' >> $(pwd)/" + cfg_name)
+            os.system("mv " + cfg_name + " config.py")
+            input("\nГотово (нажмите enter для выхода)")
+            exit(0)
+        else:
+            exit(0)
 
-        print('Загрузка twrp skkk a12')
-        os.system("aria2c https://deac-fra.dl.sourceforge.net/project/recovery-for-xiaomi-devices/alioth/twrp-3.7.0_12-v7.0_A12-alioth-skkk.img")
-        os.system("fastboot boot twrp-3.7.0_12-v7.0_A12-alioth-skkk.img")
-        print('Чтобы установить тврп на постоянку:\n1. Переходил раздел advanced (допольнительные настойки)\n2. Жмем на flash current twrp (прошить текущий тврп)')
-        input('Готово (нажмите enter для выхода)')
+    elif cfg_work == 2:
+        
+        cfg_tool = ('work_type = "firmware"')
+        os.system("echo '" + cfg_tool + "' > $(pwd)/" + cfg_name)
+        cfg_fw_path = input("Укажите расположение вашего firmware (Например /home/tux/fw.zip) : ")
+        firmw_path = ('fw_path = "' + cfg_fw_path + '"')
+        os.system("echo '" + firmw_path + "' >> $(pwd)/" + cfg_name)
+        os.system("mv " + cfg_name + " config.py")
+        input("\nГотово (нажмите enter для выхода)")
         exit(0)
+    
+    elif cfg_work == 3:
+        
+        cfg_tool = ('work_type = "rom_recovery"')
+        os.system("echo '" + cfg_tool + "' > $(pwd)/" + cfg_name)
+        cfg_rom_rec_path = input("Укажите расположение вашей прошивки (Например /home/tux/rom.zip) : ")
+        rom_rec_path = ('rom_path = "' + cfg_rom_rec_path + '"')
+        os.system("echo '" + rom_rec_path + "' >> $(pwd)/" + cfg_name)
+        os.system("mv " + cfg_name + " config.py")
+        input("\nГотово (нажмите enter для выхода)")
+        exit(0)
+    
+    elif cfg_work == 4:
 
-    elif twrp == 3:
+        cfg_tool = ('work_type = "magisk"')
+        os.system("echo '" + cfg_tool + "' > $(pwd)/" + cfg_name)
+        cfg_mgsk_file = input("Укажите источник магиска (my/download) : ")
         
-        print('Загрузка twrp skkk a13')
-        os.system("aria2c https://deac-fra.dl.sourceforge.net/project/recovery-for-xiaomi-devices/alioth/twrp-3.7.0_12-v7.0_A13-alioth-skkk.img")
-        os.system("fastboot boot twrp-3.7.0_12-v7.0_A13-alioth-skkk.img")
-        print('Чтобы установить тврп на постоянку:\n1. Переходил раздел advanced (допольнительные настойки)\n2. Жмем на flash current twrp (прошить текущий тврп)')
-        input('Готово (нажмите enter для выхода)')
-        exit(0)
-         
-    elif twrp == 4:
+        if cfg_mgsk_file == "my":
+
+            mgsk_file = ('magisk_file = "' + cfg_mgsk_file + '"')
+            os.system("echo '" + mgsk_file + "' >> $(pwd)/" + cfg_name)
+            cfg_mgsk_path = input("Укажите расположение магиска (Например /home/tux/magisk.zip: ")
+            mgsk_path = ('magisk_path = "' + cfg_mgsk_path + '"')
+            os.system("echo '" + mgsk_path + "' >> $(pwd)/" + cfg_name)
+            os.system("mv " + cfg_name + " config.py")
+            input("\nГотово (нажмите enter для выхода)")
+            exit(0)
         
-        print('Загрузка orange fox recovery stable')
-        os.system("aria2c https://us-dl.orangefox.download/63315c533c05f43c193c13b6")
-        os.system("unzip OrangeFox-alioth-stable@R11.1_1_A12.zip recovery.img")
-        os.system("fastboot boot recovery.img")
-        print('Чтобы установить orange fox recovery на постоянку:\n1. Заходим в меню (3 горизонтальные полоски)\n2. Листаем вниз и жмем на Flash current OrangeFox(прошить текущий OrangeFox)')
-        input('Готово (нажмите enter для выхода)')
-        exit(0)
-    
-    elif twrp == 5:
+        elif cfg_mgsk_file == "download":
+            
+            mgsk_file = ('magisk_file = "' + cfg_mgsk_file + '"')
+            os.system("echo '" + mgsk_file + "' >> $(pwd)/" + cfg_name)
+            cfg_mgsk_ver = input("Укажите версию магиска (stable/canary/debug) : ")
+            mgsk_ver = ('magisk_ver = "' + cfg_mgsk_ver + '"')
+            os.system("echo '" + mgsk_ver + "' >> $(pwd)/" + cfg_name)
+            os.system("mv " + cfg_name + " config.py")
+            input("\nГотово (нажмите enter для выхода)")
+            exit(0)
         
-        print('Загрузка orange fox recovery beta')
-        os.system("aria2c https://us-dl.orangefox.download/6308b4793c05f43c193bed5b")
-        os.system("unzip OrangeFox-alioth-beta@R11.1_1_5_A12.zip recovery.img")
-        os.system("fastboot boot recovery.img")
-        print('Чтобы установить orange fox recovery на постоянку:\n1. Заходим в меню (3 горизонтальные полоски)\n2. Листаем вниз и жмем на Flash current OrangeFox(прошить текущий OrangeFox)')
-        input('Готово (нажмите enter для выхода)')
-        exit(0)
-    
-    elif twrp == 6:
-        
-        print('Свое рекавери (c именем recovery.img)')
-        input('Переименуйте образ рекавери в recovery.img и нажмите enter')
-        os.system("fastboot boot recovery.img")
-        print('Чтобы установить тврп на постоянку:\n1. Переходил раздел advanced (допольнительные настойки)\n2. Жмем на flash current twrp (прошить текущий тврп)\n\nЧтобы установить orange fox recovery на постоянку:\n1. Заходим в меню (3 горизонтальные полоски)\n2. Листаем вниз и жмем на Flash current OrangeFox(прошить текущий OrangeFox)')
-        input('Готово (нажмите enter для выхода)')
-        exit(0)
+        else:
+            
+            exit(0)
     
     else:
-        
+        print("Ошибка")
         exit(0)
 
 elif tool == 2:
-    fw = int(input('1. EU 13.0.9.0\n2. Global (MI) 13.0.6.0\n3. Свой firmware (с именем fw.zip) => '))
-    
-    if fw == 1:
+    print("Выбран config.py")
+    if work_type == "recovery" and recovery_type == "twrp" and show_guide == "true":
         
-        print("Вы выбрали - EU 13.0.9.0")
-        print("Перейдите в режим рекавери и запустите adb sideload")
-        os.system("aria2c https://bot.siddrive.ga/0:/fw_alioth_miui_ALIOTHEEAGlobal_V13.0.9.0.SKHEUXM_f9603cf7f3_12.0.zip")
-        os.system("adb wait-for-device")
-        os.system("adb sideload fw_alioth_miui_ALIOTHEEAGlobal_V13.0.9.0.SKHEUXM_f9603cf7f3_12.0.zip")
-        input("Готово (нажмите enter для выхода)")
+        input("Перейдите в режим fastboot и нажмите enter")
+        print("\nрасположение рекавери -", recovery_path, "\nтип рекавери -", recovery_type, "\nпоказывать гайд по прошивке рекавери после установки - ", show_guide, "\n")
+        input("Убедитесь, что все правильно и нажмите enter")
+        os.system("fastboot boot", recovery_path)
+        print('Чтобы установить тврп на постоянку:\n1. Переходил раздел advanced (допольнительные настойки)\n2. Жмем на flash current twrp (прошить текущий тврп)')
+        input("\nГотово (нажмите enter для выхода)")
         exit(0)
-    
-    elif fw == 2:
 
-        print("Вы выбрали - Global (MI) 13.0.6.0")
-        print("Перейдите в режим рекавери и запустите adb sideload")
-        os.system("aria2c https://void.siddrive.ga/0:/fw_alioth_miui_ALIOTHGlobal_V13.0.6.0.SKHMIXM_e83b44e411_12.0.zip")
-        os.system("adb wait-for-device")
-        os.system("adb sideload fw_alioth_miui_ALIOTHGlobal_V13.0.6.0.SKHMIXM_e83b44e411_12.0.zip")
-        input("Готово (нажмите enter для выхода)")
-        exit(0)
-   
-    elif fw == 3:
+    elif work_type == "recovery" and recovery_type == "twrp" and show_guide == "false":
         
-        print("Поместите ваш firmware с именем fw.zip")
-        input("Чтобы продолжить нажмите enter")
-        print("Перейдите в режим рекавери и запустите adb sideload")
-        os.system("adb wait-for-device")
-        os.system("adb sideload fw.zip")
-        input("Готово (нажмите enter для выхода)")
+        input("Перейдите в режим fastboot и нажмите enter")
+        print("\nрасположение рекавери -", recovery_path, "\nтип рекавери -", recovery_type, "\nпоказывать гайд по прошивке рекавери после установки - ", show_guide, "\n")
+        input("Убедитесь, что все правильно и нажмите enter")
+        os.system("fastboot boot", recovery_path)
+        input("\nГотово (нажмите enter для выхода)")
+        exit(0)
+            
+    elif work_type == "recovery" and recovery_type == "ofox" and show_guide == "true":
+
+        print("\nрасположение рекавери -", recovery_path, "\nтип рекавери -", recovery_type, "\nпоказывать гайд по прошивке рекавери после установки -", show_guide, "\nпапка для распаковки -", unpackfolder, "\n")
+        input("Убедитесь, что все правильно и нажмите enter")
+        os.system("unzip " + recovery_path + " -d " + unpackfolder + " && cd " + unpackfolder + " && fastboot boot recovery.img")        
+        print('Чтобы установить orange fox recovery на постоянку:\n1. Заходим в меню (3 горизонтальные полоски)\n2. Листаем вниз и жмем на Flash current OrangeFox(прошить текущий OrangeFox)')
+        input("\nГотово (нажмите enter для выхода)")
+        exit(0) 
+    
+    elif work_type == "recovery" and recovery_type == "ofox" and show_guide == "false":
+
+        print("\nрасположение рекавери -", recovery_path, "\nтип рекавери -", recovery_type, "\nпоказывать гайд по прошивке рекавери после установки -", show_guide, "\nпапка для распаковки -", unpackfolder, "\n")
+        input("Убедитесь, что все правильно и нажмите enter")
+        os.system("unzip " + recovery_path + " -d " + unpackfolder + " && cd " + unpackfolder + " && fastboot boot recovery.img")
+        input("\nГотово (нажмите enter для выхода)")
+        exit(0)
+
+    elif work_type == "firmware":
+        
+        print("\nрасположение firmware -", fw_path, "\n")
+        input("Убедитесь, что все правильно и нажмите enter\n")
+        input("Перейдите в режим рекавери и запустите adb sideload и нажмите enter")
+        os.system("adb sideload " + fw_path)
+        input("\nГотово (нажмите enter для выхода)")
         exit(0)
     
+    elif work_type == "rom_recovery":
+        
+        print("\nрасположение прошивки (recovery) -", rom_path, "\n")
+        input("Убедитесь, что все правильно и нажмите enter\n")
+        input("Перейдите в режим рекавери и запустите adb sideload и нажмите enter")
+        os.system("adb sideload " + rom_path)
+        input("\nГотово (нажмите enter для выхода)")
+        exit(0)
+    
+    elif work_type == "magisk" and magisk_file == "my":
+            
+        print("\nисточник магиска -", magisk_file, "\nрасположение магиска -", magisk_path, "\n")
+        input("Убедитесь, что все правильно и нажмите enter\n")
+        input("Перейдите в режим рекавери и запустите adb sideload и нажмите enter")
+        os.system("adb sideload " + magisk_path)
+        
+    elif work_type == "magisk" and magisk_file == "download" and magisk_ver == "stable":
+            
+        print("\nисточник магиска -", magisk_file, "\nверсия магиска -", magisk_ver, "\n")
+        input("Убедитесь, что все правильно и нажмите enter\n")
+        input("Перейдите в режим рекавери и запустите adb sideload и нажмите enter")
+        os.system("aria2c https://github.com/topjohnwu/Magisk/releases/download/v25.2/Magisk-v25.2.apk " + "&& mv Magisk-v25.2.apk Magisk-v25.2.zip && adb sideload Magisk-v25.2.zip")
+        input("\nГотово (нажмите enter cfg_toolдля выхода)")
+        exit(0)
+    
+    elif work_type == "magisk" and magisk_file == "download" and magisk_ver == "canary":
+            
+        print("\nисточник магиска -", magisk_file, "\nверсия магиска -", magisk_ver, "\n")
+        input("Убедитесь, что все правильно и нажмите enter\n")
+        input("Перейдите в режим рекавери и запустите adb sideload и нажмите enter")
+        os.system("aria2c https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-release.apk " + "&& mv app-release.apk Magisk-Canary.zip && adb sideload Magisk-Canary.zip")
+        input("\nГотово (нажмите enter для выхода)")
+        exit(0)
+    
+    elif work_type == "magisk" and magisk_file == "download" and magisk_ver == "debug":
+            
+        print("\nисточник магиска -", magisk_file, "\nверсия магиска -", magisk_ver, "\n")
+        input("Убедитесь, что все правильно и нажмите enter\n")
+        input("Перейдите в режим рекавери и запустите adb sideload и нажмите enter")
+        os.system("aria2c https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk " + "&& mv app-debug.apk Magisk-Debug.zip && adb sideload Magisk-Debug.zip")
+        input("\nГотово (нажмите enter для выхода)")
+        exit(0)
+        
     else:
-        
-        exit(0)
-
-elif tool == 3:
-   
-    print('Вы выбрали - "Зашив прошивки (для recovery)"')
-    print("Переименуйте файл прошивки в rom.zip")
-    input("Чтобы продолжить нажмите enter")
-    print("Перейдите в режим рекавери и запустите adb sideload")
-    os.system("adb wait-for-device")
-    os.system("adb sideload rom.zip")
-    input("Готово (нажмите enter для выхода)")
-    exit(0)
-
-elif tool == 4:
-    
-    print('Вы выбрали - "Магиск"')
-    mgsk = int(input("1. - Magisk Stable v25.2 \n2. - Magisk Canary\n3. - Magisk Debug\n4. - Свой магиск => "))
-    
-    if mgsk == 1:
-
-        print('Magisk Stable v25.2')
-        os.system("aria2c https://github.com/topjohnwu/Magisk/releases/download/v25.2/Magisk-v25.2.apk")
-        print("Перейдите в режим рекавери и запустите adb sideload")
-        os.system("mv Magisk-v25.2.apk Magisk-v25.2.zip")
-        input("Чтобы продолжить нажмите enter")
-        os.system("adb wait-for-device")
-        os.system("adb sideload Magisk-v25.2.zip")
-        input("Готово (нажмите enter для выхода)")
-        exit(0)
-
-    elif mgsk == 2:
-
-        print('Magisk Canary')
-        os.system("aria2c https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-release.apk")
-        print("Перейдите в режим рекавери и запустите adb sideload")
-        os.system("mv app-release.apk Magisk-Canary.zip")
-        input("Чтобы продолжить нажмите enter")
-        os.system("adb wait-for-device")
-        os.system("adb sideload Magisk-Canary.zip")
-        input("Готово (нажмите enter для выхода)")
-        exit(0)
-
-    elif mgsk == 3:
-
-        print('Magisk Debug')
-        os.system("aria2c https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk")
-        print("Перейдите в режим рекавери и запустите adb sideload")
-        os.system("mv app-debug.apk Magisk-Debug.zip")
-        input("Чтобы продолжить нажмите enter")
-        os.system("adb wait-for-device")
-        os.system("adb sideload Magisk-Debug.zip")
-        input("Готово (нажмите enter для выхода)")
-        exit(0)
-
-    elif mgsk == 4:
-
-        print("Поместите Magisk с именем magisk.zip")
-        print("Перейдите в режим рекавери и запустите adb sideload")
-        input("Чтобы продолжить нажмите enter")
-        os.system("adb wait-for-device")
-        os.system("adb sideload magisk.zip")
-        input("Готово (нажмите enter для выхода)")
-        exit(0)
-
-    else:
-
-        exit(0)
+            
+        input("Ошибка! Проверьте файл config.py")
+        exit(0)  
 
 else:
     
-    exit(0)       
-    
-
-        
-
+    exit(0)
